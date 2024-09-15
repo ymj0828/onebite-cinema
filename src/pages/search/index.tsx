25b452ef48b0f7256778e572d6d44d113b4cbaa4
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-09-15 23:30:10 +0900

    feat: 서치 페이지 SSR로 데이터 페칭

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,16 +1,28 @@
-import movies from '@/mock/dummy.json';
-
 import { ReactNode } from 'react';
 
 import style from './search.module.css';
 import classNames from 'classnames/bind';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
 import MovieItem from '@/components/movie-item';
 import SearchableLayout from '@/components/searchable-layout';
 
+import fetchMovies from '@/lib/fetch-movies';
+
 const cx = classNames.bind(style);
 
-export default function Page() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
+
+  return {
+    props: {
+      movies,
+    },
+  };
+};
+
+export default function Page({ movies }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={cx('container')}>
       {movies.map((movie) => (
